db8a3596b36c40e8f68a41113f7bda3b1211b064
efi kater <efi.kater@hellofyllo.com>
2023-01-09 10:59:08 +0200

    remain apiMain to playground and delete its content

---
+++ src/main/java/PlayGroudMain.java
@@ -0,0 +1,10 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+public class PlayGroudMain {
+    public static void main(String[] args)  {
+
+
+        }
+}
